3679e958f8855991f9536e66c0fd10a8d7bbe9c3
Balasridhar143 <balasridharp@gmail.com>
2026-07-23 05:06:09 +0530

    cmt

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.database.mongodb import mongodb
+
 from app.api.routes.auth_routes import router as auth_router
 from app.api.routes.recruiter_routes import router as recruiter_router
 from app.api.routes.company_routes import router as company_router
@@ -9,6 +13,8 @@ from app.api.routes.internship_routes import router as internship_router
 from app.api.routes.hackathon_routes import router as hackathon_router
 from app.api.routes.resume_routes import router as resume_router
 from app.api.routes.ai_routes import router as ai_router
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await mongodb.connect()
@@ -22,46 +28,77 @@ app = FastAPI(
     version="1.0.0",
     lifespan=lifespan,
 )
+
+# -------------------------------
+# CORS Configuration
+# -------------------------------
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# -------------------------------
+# Routers
+# -------------------------------
+
 app.include_router(
     auth_router,
     prefix="/auth",
     tags=["Authentication"]
 )
+
 app.include_router(
     recruiter_router,
     prefix="/recruiter",
     tags=["Recruiter"]
 )
+
 app.include_router(
     company_router,
     prefix="/company",
     tags=["Company"]
 )
+
 app.include_router(
     job_router,
     prefix="/job",
     tags=["Jobs"]
 )
+
 app.include_router(
     internship_router,
     prefix="/internship",
     tags=["Internships"]
 )
+
 app.include_router(
     hackathon_router,
     prefix="/hackathon",
     tags=["Hackathons"]
 )
+
 app.include_router(
     resume_router,
     prefix="/resume",
     tags=["Resume"]
 )
+
 app.include_router(
     ai_router,
     prefix="/ai",
     tags=["AI"]
 )
+
 
 @app.get("/")
 async def home():
